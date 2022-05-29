d0958cc3f7fc6b645124ec15cf94f2a23f421f91
iShapeNoise <contact@jensmeisner.net>
2022-05-29 02:20:43 +0200

    Debug Midi.py

--- FoxDot/lib/Midi.py
+++ FoxDot/lib/Midi.py
@@ -35,7 +35,8 @@ class MidiInputHandler(object):
         self.tt_time = time.time()
         self.tt_ptime = self.tt_time
         self.msg = [0, 0, 0]
-        self.msg_list = []
+        self.n_msg_list = []
+        self.c_msg_list = []
         self.print_msg = False
 
     def __call__(self, event, data=None):
@@ -48,55 +49,42 @@ class MidiInputHandler(object):
             print(self.msg)
 
         if(self.msg[0] == 144 or self.msg[0] == 145):
-            if len(self.msg_list) == 0:
-                self.msg_list.append(self.msg)
+            if len(self.n_msg_list) == 0:
+                self.n_msg_list.append(self.msg)
             else:
-                if self.msg not in self.msg_list:
-                    self.msg_list.append(self.msg)
-        elif self.msg[0] == 128:
-            if len(self.msg_list) > 0:
-                for count, item in enumerate(self.msg_list):
+                if self.msg not in self.n_msg_list:
+                    self.n_msg_list.append(self.msg)
+        elif self.msg[0] == 128 or self.msg[0] == 129:
+            if len(self.n_msg_list) > 0:
+                for count, item in enumerate(self.n_msg_list):
                     if self.msg[1] == item[1]:
-                        self.msg_list.remove(self.msg_list[count])
-        elif self.msg[0] == 129:
-            for count, item in enumerate(self.msg_list):
-                if self.msg[1] == item[1]:
-                    self.msg_list[count][2] = 0
+                        self.n_msg_list.remove(self.n_msg_list[count])
         else:
-            if len(self.msg_list) == 0:
-                self.msg_list.append(self.msg)
-            else:
-                if self.msg not in self.msg_list:
-                    self.msg_list.append(self.msg)
-                else:
-                    for count, item in enumerate(self.msg_list):
-                        if self.msg[1] == item[1]:
-                            self.msg_list.remove(self.msg_list[count])
-                            self.msg_list.append(self.msg)
-
-        #print(self.msg_list)
+            if len(self.c_msg_list) == 0:
+                self.c_msg_list.append(self.msg)
+            elif self.msg not in self.c_msg_list:
+                self.c_msg_list.append(self.msg)
+            for count, item in enumerate(self.c_msg_list):
+                if self.msg[0] == item[0] and self.msg[1] == item[1] and self.msg[2] != item[2]:
+                    self.c_msg_list.remove(self.c_msg_list[count])
+                elif self.msg[0] == item[0] and self.msg[1] == item[1]:
+                    self.c_msg_list[count] = self.msg
 
         if self.tt and message[0] == 128 and message[1] == 0:
             self.tt_time = time.time()
             if self.tt_ptime < self.tt_time:
                 self.tt_bpm = (1/(self.tt_time - self.tt_ptime)) * 60
                 self.tt_ptime = self.tt_time
-
+        #
         self.midi_ctrl.delta += delta
-
         #if TIMING_CLOCK in datatype and not self.played:
         if not self.played:
             self.midi_ctrl.pulse += 1
-
             if self.midi_ctrl.pulse == self.midi_ctrl.ppqn:
-
                 t_master = 60.0
-
                 self.midi_ctrl.bpm = round(60.0 / self.midi_ctrl.delta, 0)
-
                 self.midi_ctrl.pulse = 0
                 self.midi_ctrl.delta = 0.0
-
                 #print("CONTROLLER BPM : " + repr(self.midi_ctrl.bpm))
 
 
@@ -107,21 +95,15 @@ class MidiIn:
         """ Class for listening for MIDI clock messages
             from a midi device """
         try:
-
             self.device = rtmidi.MidiIn()
-
         except NameError:
-
             raise ImportError(_err)
 
         self.available_ports = self.device.get_ports()
 
         if not self.available_ports:
-
             raise MIDIDeviceNotFound
-
         else:
-
             print("MidiIn: Connecting to " + self.available_ports[port_id])
 
         self.device.open_port(port_id)
@@ -146,43 +128,50 @@ class MidiIn:
         return
 
     def tempo_tapper(self, tt_bool):
+        """ Activate Tempo Tapper Device """
         self.handler.tt = tt_bool
         return
 
     def tempo_tapper_bpm(self):
+        """ Get Beats per Minute (bpm) value """
         self.bpm = self.handler.tt_bpm
         return self.bpm
 
     def get_ctrl(self, channel):
-        for i in range(len(self.handler.msg_list)):
-            if self.handler.msg_list[i][1] == channel:
-                self.ctrl_value = self.handler.msg_list[i][2]
+        """ Get control value from knobs / pads """
+        for i in range(len(self.handler.c_msg_list)):
+            if self.handler.c_msg_list[i][1] == channel:
+                self.ctrl_value = self.handler.c_msg_list[i][2]
         return self.ctrl_value
 
     def get_note(self):
+        """ Get midinote from keys"""
         self.note = ()
-        if len(self.handler.msg_list) > 0:
-            for i in range(len(self.handler.msg_list)):
-                self.note = self.note + (self.handler.msg_list[i][1],)
+        if len(self.handler.n_msg_list) > 0:
+            for i in range(len(self.handler.n_msg_list)):
+                self.note = self.note + (self.handler.n_msg_list[i][1], )
         else:
             self.note = self.note + (0, )
         return self.note
 
     def get_velocity(self):
+        """ Get velocity of midi keys / pads """
         self.velocity = ()
-        if len(self.handler.msg_list) > 0:
-            for i in range(len(self.handler.msg_list)):
+        if len(self.handler.n_msg_list) > 0:
+            for i in range(len(self.handler.n_msg_list)):
                 self.velocity = self.velocity + \
-                    (self.handler.msg_list[i][2] / 64, )
+                    (self.handler.n_msg_list[i][2] / 64, )
         else:
             self.velocity = (0, )
         return self.velocity
 
     def get_delta(self):
+        """ Get delta """
         self.delta = self.handler.delta
         return self.delta
 
     def print_message(self, boolmsg):
+        """ Print incoming midi message"""
         self.handler.print_msg = boolmsg
         return self.handler.print_msg
 
@@ -200,7 +189,6 @@ class MidiOut(SynthDefProxy):
 
 
 midi = MidiOut  # experimental alias
-
 # Midi information exceptions
 
 
@@ -215,5 +203,4 @@ class rtMidiNotFound(Exception):
 
 
 if __name__ == "__main__":
-
     a = MidiIn()
