78545a7b65a7c19d8694b8c178be5a7e71b7077d
Elie Gavoty <eliegavoty@free.fr>
2022-07-02 16:10:45 +0200

    ParamMatrix WIP

--- FoxDot/preset/common/ParamMatrix.py
+++ FoxDot/preset/common/ParamMatrix.py
@@ -1,4 +1,6 @@
 from FoxDot import Pvar
+from typing import List
+
 
 class ParamRange:
 
@@ -41,13 +43,30 @@ class ParamRange:
                 ParamRange(other.start, other.stop, other.value),
                 ParamRange(other.stop, self.stop, self.value),
             ]
-        elif other.start < self.start and other.stop < self.stop:
+        elif other.start <= self.start and other.stop < self.stop:
             return [
                 ParamRange(other.start, other.stop, other.value),
                 ParamRange(other.stop, self.stop, self.value),
             ]
+        elif self.stop == other.start:
+            return [
+                ParamRange(self.start, self.stop, self.value),
+                ParamRange(other.start, other.stop, other.value)
+            ]
+        elif self.start == other.stop:
+            return [
+                ParamRange(other.start, other.stop, other.value),
+                ParamRange(self.start, self.stop, self.value),
+            ]
         else:
-            raise Exception()
+            raise Exception("Error in ParamRange boolean operation")
+
+def uniq(l:List):
+    res = []
+    for x in l:
+        if x not in res:
+            res.append(x)
+    return res
 
 class ParamTimeline:
 
@@ -64,18 +83,37 @@ class ParamTimeline:
                 new_param_ranges += pr.boolean_union(new_pr)
             else:
                 new_param_ranges.append(pr)
-        self.param_ranges = list(set(new_param_ranges))
-        self.param_ranges.sort()
-
-    def __add__(self, other):
-        assert isinstance(other, tuple) and len(other) == 3, "add operator for param range works with tuples of length 3"
+        new_param_ranges = uniq(new_param_ranges)
+        new_param_ranges.sort()
+        self.param_ranges = new_param_ranges
+        for i, pr in enumerate(self.param_ranges): # merge Param Ranges contiguous with same value by overwriting with a new PR
+            if i < len(self.param_ranges)-1:
+                if self.param_ranges[i].value == self.param_ranges[i+1].value:
+                    self.add_value_range(ParamRange(self.param_ranges[i].start, self.param_ranges[i+1].stop, self.param_ranges[i].value))
+        return self
+    def __lshift__(self, other):
+        assert isinstance(other, tuple) and len(other) == 3, "lshift operator for param timeline works with tuples of length 3"
         self.add_value_range(ParamRange(other[0], other[1], other[2]))
         return self
-
+    def __mul__(self, other):
+        if isinstance(other, int):
+            old_duration = self.duration
+            self.duration *= other
+            for i in range(1,other):
+                for pr in self.param_ranges:
+                    new_pr = ParamRange(
+                        start=pr.start + i*old_duration,
+                        stop=pr.stop + i*old_duration,
+                        value=pr.value,
+                    )
+                    self.add_value_range(new_pr)
+        return self
     def __repr__(self):
         return f"<ParamTimeline {self.param_ranges}>"
 
     def out(self):
+        if len(self.param_ranges) == 1:
+            return self.param_ranges[0].value
         values = [pr.value for pr in self.param_ranges]
         durations = [pr.stop - pr.start for pr in self.param_ranges]
         return Pvar(values, durations)
@@ -85,5 +123,51 @@ class ParamTimeline:
 Pt = ParamTimeline
 
 
+class ParamMatrix:
+    def __init__(self, duration, *args, **kwargs):
+        self.duration = duration
+        # self.params = { # default values
+        #     "degree": P[0],
+        #     "dur": P[1],
+        #     "amp": P[1],
+        # }
+        # problem with default values in matrix is that it blocks the usage of manual params in addition to the matrix
+        self.params = { key: ParamTimeline(duration, value) for key, value in kwargs.items() }
+        # self.params["sus"] = self.params["dur"] if 'sus' not in self.params.keys() else self.params["sus"]
+    def __lshift__(self, other):
+        assert isinstance(other, ParamVector), "lshift operator for param matrix works with a ParamVector"
+        for key, value in other.params.items():
+            if key in self.params.keys():
+                start = other.start if other.start is not None else 0
+                stop = other.stop if other.stop is not None else self.duration
+                self.params[key].add_value_range(ParamRange(
+                    start,
+                    stop,
+                    value
+                ))
+            else: # if the param does not exist in matrix (no value nor default value) add its value to the whole timeline
+                self.params[key] = ParamTimeline(self.duration, value)
+        return self
+    def __mul__(self, other):
+        if isinstance(other, int):
+            self.duration = self.duration * other
+            for key, value in self.params.items():
+                res = value * other
+                self.params[key] = res
+        return self
+    def __repr__(self):
+        return f"<ParamMatrix { { key:value for key, value in self.params.items()} }>"
+    def out(self):
+        res = { key: _pt.out() for key, _pt in self.params.items() }
+        return res
+Pm = ParamMatrix
 
 
+class ParamVector:
+    def __init__(self, start=None, stop=None, *args, **kwargs):
+        self.start = start
+        self.stop = stop
+        self.params = { key: value for key, value in kwargs.items() }
+
+
+Pv = ParamVector
