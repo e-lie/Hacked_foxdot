c484fc19bb362854ba5ee97fa7d14790c6eb2bc6
Elie Gavoty <eliegavoty@free.fr>
2022-09-09 12:18:21 +0200

    fix

--- FoxDot/lib/Extensions/ReaperIntegrationLib/ReapyExtensions.py
+++ FoxDot/lib/Extensions/ReaperIntegrationLib/ReapyExtensions.py
@@ -11,34 +11,40 @@ def move_fx(src_track, dest_track):
         RPR.TrackFX_CopyToTrack(src_track, fx_to_move_index, dest_track, RPR.TrackFX_GetCount(dest_track), True)
 
 def add_chunk_to_track(track, chunk):  # add empty fx chain chunk if not exists
-    track_xml_chunk = RPR.GetTrackStateChunk(track, '', 999999, False)[2]
-    #chunk_ch = track_chunk[2]
-    if 'FXCHAIN' not in track_xml_chunk:
-        track_xml_chunk = track_xml_chunk[0:-3] + '\n<FXCHAIN\nSHOW 0\nLASTSEL 0\n DOCKED 0\n>\n>\n'
-    if chunk:
-        track_xml_chunk = track_xml_chunk.replace('DOCKED 0', 'DOCKED 0\n ' + chunk)
-    RPR.SetTrackStateChunk(track, track_xml_chunk, False)
+    try:
+        track_xml_chunk = RPR.GetTrackStateChunk(track, '', 999999, False)[2]
+        #chunk_ch = track_chunk[2]
+        if 'FXCHAIN' not in track_xml_chunk:
+            track_xml_chunk = track_xml_chunk[0:-3] + '\n<FXCHAIN\nSHOW 0\nLASTSEL 0\n DOCKED 0\n>\n>\n'
+        if chunk:
+            track_xml_chunk = track_xml_chunk.replace('DOCKED 0', 'DOCKED 0\n ' + chunk)
+        RPR.SetTrackStateChunk(track, track_xml_chunk, False)
+    except:
+        print("Error initializing Reapy Extensions : is reaper started ?")
 
 
 def add_fx_chain(track, chain_name):
-    track = track.id
-    fxchain_filepath = RPR.GetResourcePath() + '/FXChains/' + f'{chain_name}.RfxChain'
+    try:
+        track = track.id
+        fxchain_filepath = RPR.GetResourcePath() + '/FXChains/' + f'{chain_name}.RfxChain'
 
-    if RPR.file_exists(fxchain_filepath):
-        with open(fxchain_filepath, "r") as f:
-            content = f.read()
+        if RPR.file_exists(fxchain_filepath):
+            with open(fxchain_filepath, "r") as f:
+                content = f.read()
 
-    RPR.PreventUIRefresh(1)
-    RPR.InsertTrackAtIndex(RPR.CountTracks(0), False)
-    last_track = RPR.GetTrack(0,RPR.CountTracks(0)-1)
+        RPR.PreventUIRefresh(1)
+        RPR.InsertTrackAtIndex(RPR.CountTracks(0), False)
+        last_track = RPR.GetTrack(0,RPR.CountTracks(0)-1)
 
-    add_chunk_to_track(last_track, content)
+        add_chunk_to_track(last_track, content)
 
-    move_fx(last_track, track)
-    RPR.DeleteTrack(last_track)
-    RPR.PreventUIRefresh(-1)
+        move_fx(last_track, track)
+        RPR.DeleteTrack(last_track)
+        RPR.PreventUIRefresh(-1)
+    except:
+        print("Error initializing Reapy Extensions : is reaper started ?")
 
-pproject = Project()
+#pproject = Project()
 
 #track = pproject.tracks["chan1"]
 
